ea25cbbd9128a504f8bb4ae26759fd5a5251b078
KinglerChamp <130119311+KinglerChamp@users.noreply.github.com>
2023-07-29 11:07:12 -0400

    Update shared_controls.js

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -345,9 +345,6 @@ var lastManualStatus = {"#p1": "Healthy"};
 var lastAutoStatus = {"#p1": "Healthy"};
 function autosetStatus(p, item) {
 	var currentStatus = $(p + " .status").val();
-	if (currentStatus !== lastAutoStatus[p]) {
-		lastManualStatus[p] = currentStatus;
-	}
 	if (item === "Flame Orb") {
 		lastAutoStatus[p] = "Burned";
 		$(p + " .status").val("Burned");
@@ -356,12 +353,6 @@ function autosetStatus(p, item) {
 		lastAutoStatus[p] = "Badly Poisoned";
 		$(p + " .status").val("Badly Poisoned");
 		$(p + " .status").change();
-	} else {
-		lastAutoStatus[p] = "Healthy";
-		if (currentStatus !== lastManualStatus[p]) {
-			$(p + " .status").val(lastManualStatus[p]);
-			$(p + " .status").change();
-		}
 	}
 }
 
@@ -461,6 +452,25 @@ function smogonAnalysis(pokemonName) {
 // auto-update set details on select
 $(".set-selector").change(function () {
 	var fullSetName = $(this).val();
+	
+	if ($(this).hasClass('opposing')) {
+			CURRENT_TRAINER_POKS = get_trainer_poks(fullSetName)
+
+
+	var next_poks = CURRENT_TRAINER_POKS
+
+	var trpok_html = ""
+	for (i in next_poks ) {
+		if (next_poks[i][0].includes($('input.opposing').val())){
+				continue
+			}
+		var pok_name = next_poks[i].split(" ")[0]
+		var pok = `<img class="trainer-pok right-side" src="https://raw.githubusercontent.com/May8th1995/sprites/master/${pok_name}.png" data-id="${CURRENT_TRAINER_POKS[i].split("[")[0]}" title="${next_poks[i]}, ${next_poks[i]} BP">`
+			trpok_html += pok
+		}
+	}
+
+	$('.trainer-pok-list-opposing').html(trpok_html)
 	var pokemonName = fullSetName.substring(0, fullSetName.indexOf(" ("));
 	var setName = fullSetName.substring(fullSetName.indexOf("(") + 1, fullSetName.lastIndexOf(")"));
 	var pokemon = pokedex[pokemonName];
@@ -1387,6 +1397,74 @@ function loadCustomList(id) {
 		}
 	});
 }
+function get_trainer_names() {
+    var all_poks = SETDEX_SS
+    var trainer_names = [] 
+
+    for (const [pok_name, poks] of Object.entries(all_poks)) {
+        var pok_tr_names = Object.keys(poks)
+        for (i in pok_tr_names) {
+           var trainer_name = pok_tr_names[i]
+           trainer_names.push(`${pok_name} (${trainer_name})`) 
+        }      
+    }
+    return trainer_names
+}
+
+function get_box() {
+    var names = get_trainer_names()
+
+    var box = []
+
+    var box_html = ""
+
+    for (i in names) {
+        if (names[i].includes("Custom")) {
+            box.push(names[i].split("[")[0])
+
+            var pok_name = names[i].split(" (")[0]
+            var pok = `<img class="trainer-pok left-side" src="https://raw.githubusercontent.com/May8th1995/sprites/master/${pok_name}.png" data-id="${names[i].split("[")[0]}">`
+
+            box_html += pok
+        }   
+    }
+
+    $('.player-poks').html(box_html)
+
+
+    return box
+}
+
+function get_trainer_poks(trainer_name)
+{
+
+	var true_name = trainer_name.split("(")[1]
+    var matches = []
+    for (i in TR_NAMES) {
+        if (TR_NAMES[i].includes(true_name)) {
+            matches.push(TR_NAMES[i])
+        }
+    }
+    return matches
+}
+
+$(document).on('click', '.right-side', function() {
+	var set = $(this).attr('data-id')
+	$('.opposing').val(set)
+	console.log("hit")
+
+	$('.opposing').change()
+	$('.opposing .select2-chosen').text(set)
+})
+
+$(document).on('click', '.left-side', function() {
+	var set = $(this).attr('data-id')
+	$('.player').val(set)
+
+	$('.player').change()
+	$('.player .select2-chosen').text(set)
+	get_box()
+})
 
 $(document).ready(function () {
 	var params = new URLSearchParams(window.location.search);
