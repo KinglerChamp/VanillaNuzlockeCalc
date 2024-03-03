ca903818ba11e216fcb17336c7f8e7773cc635a3
Hashir <hashirbusinessinfo@gmail.com>
2024-03-04 03:05:06 +0500

    updated UI

---
+++ js/update.js
@@ -0,0 +1,6 @@
+const importExport = document.querySelector('.import-export')
+const pokeReport = document.querySelector('.poke-import ')
+
+importExport.addEventListener('click', () => {
+    pokeReport.classList.toggle('active')
+})
