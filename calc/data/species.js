5617b8029cfc2d1cfeee06345a8f71f15d309c09
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2025-02-25 11:15:30 -0500

    Update species.js

--- calc/data/species.js
+++ calc/data/species.js
@@ -10675,12 +10675,13 @@ var LE_PATCH = {
 
 var SV = (0, util_1.extend)(true, {}, SS, SV_PATCH, PLA_PATCH, LE_PATCH);
 
-Object.keys(SV).forEach(key => {
-    if (!SV[key].nfe) {
-        delete SV[key];
-        console.log(SV[key]);
-    }
-});
+//Object.keys(SV).forEach(key => {
+//    if (!SV[key].nfe) {
+//        delete SV[key];
+//        console.log(SV[key]);
+//    }
+//});
+delete SV['Abomasnow'];
 
 exports.SPECIES = [{}, RBY, GSC, ADV, DPP, BW, XY, SM, SS, SV];
 var Species = (function () {
