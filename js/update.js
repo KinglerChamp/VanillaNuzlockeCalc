cf65416ace37b13ff90050103710b6f58d2d041c
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2026-02-21 07:49:08 -0500

    c

--- js/update.js
+++ js/update.js
@@ -109,3 +109,14 @@ setTimeout(() => {
     });
 
 }, 1000);
+
+$(document).keydown(async function (e) {
+    console.log(e.altKey, e.key);
+    if ((e.altKey) && e.key == 'c') {
+        console.log(1);
+        $("#critR1")[0].checked = !$("#critR1")[0].checked
+        $("#critR2")[0].checked = !$("#critR2")[0].checked
+        $("#critR3")[0].checked = !$("#critR3")[0].checked
+        $("#critR4")[0].checked = !$("#critR4")[0].checked
+    }
+});
